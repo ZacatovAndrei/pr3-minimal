98fa2536bab4945ea2ff73508bf59685bdf8e464
Zacatov Andrei <andrei.zacatov@isa.utm.md>
2022-11-29 19:48:01 +0200

    Fix create

--- create.go
+++ create.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -8,7 +9,7 @@ import (
 	"time"
 )
 
-func adjustname(unit *DataUnit) {
+func adjustName(unit *DataUnit) {
 	var newNum = 1
 	FileBaseName := unit.FileName + "_"
 	for {
@@ -30,20 +31,32 @@ func createFile(w http.ResponseWriter, r *http.Request) {
 	if ok != nil {
 		panic(ok)
 	}
+	//if a request is empty then return from it with "bad request" code 400
+	if len(bytes) == 0 {
+		w.WriteHeader(400)
+		fmt.Fprintln(w, "Empty request")
+		return
+	}
 	// The whole part of adding a file to the storage
 	data := deserializeDataUnit(bytes)
+	if data.FileName == "" {
+		//if the json has no filename in it
+		//return from it with "bad request" code 400
+		w.WriteHeader(400)
+		fmt.Fprintln(w, "No filename provided")
+	}
 	data.timeStamp = time.Now().Unix()
 	//checking if a file with that name exists
 	if _, exists := storage[data.FileName]; exists {
 		log.Println("Name already exists, a new name will be automatically assigned")
-		adjustname(&data)
+		adjustName(&data)
 	}
 	storage[data.FileName] = data
 	log.Println("Stored file ", data.FileName)
 }
 
 /*
-	just in case i decide to add those guards
+	just in case I decide to add those guards
 	if r.Method != "POST" {
 		w.WriteHeader(405)
 		_, _ = fmt.Fprintf(w, "Method not supported")
